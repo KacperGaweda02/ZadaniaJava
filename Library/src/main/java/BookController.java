415e08e6d13041e4204f2e77054f9de1325f0703
KacperGaweda02 <105288861+KacperGaweda02@users.noreply.github.com>
2022-05-17 21:05:48 +0200

    zadania
    poprawa zadań 1 i 2

---
+++ Library/src/main/java/BookController.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class BookController {
+    // to jest kontroler
+    public static Scanner scanner = new Scanner(System.in);
+    public static String input;
+    public static String bookToRemove;
+    public static String bookToSee;
+    public static String newTitle;
+    public static String newAuthor;
+    public static String newGenre;
+    public static String newDescrip;
+    public static String blankValue;
+    public static void letsGetStarted() {
+        BookView.greeting();
+        BookView.showMenu();
+        askAndGive();
+    }
+    public static void askAndGive() {
+        input = scanner.next();
+        BookService.doSomethingWithIt(input);
+    }
+    public static void addBook() {
+        System.out.println("What's the title?");
+        blankValue = scanner.nextLine();
+        newTitle = scanner.nextLine();
+        System.out.println("Who is the author?");
+        newAuthor = scanner.nextLine();
+        System.out.println("What's the genre?");
+        newGenre = scanner.nextLine();
+        System.out.println("What's the description?");
+        newDescrip = scanner.nextLine();
+        BookService.createNewBook(newTitle, newAuthor, newGenre, newDescrip);
+    }
+    public static void getBookToRemove() {
+        blankValue = scanner.nextLine();
+        bookToRemove = scanner.nextLine();
+        BookService.removeBook(bookToRemove);
+    }
+    public static void getSpecBook() {
+        blankValue = scanner.nextLine();
+        bookToSee = scanner.nextLine();
+        BookService.showInfo(bookToSee);
+    }
+}
