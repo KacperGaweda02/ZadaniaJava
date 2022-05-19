efd7e4b401644b3ce66d26255b6df6ef447eedd6
KacperGaweda02 <105288861+KacperGaweda02@users.noreply.github.com>
2022-05-19 15:26:38 +0200

    poprawa zadania 1 i 2
    zrobiłem wszystko tylko dalej nie trynie tego patternu DTO więc go jeszcze nie zrobiłem.

---
+++ Library/src/main/java/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    String title;
+    String author;
+    String genre;
+    String descrip;
+    public Book(String title, String author, String genre, String descrip) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.descrip = descrip;
+    }
+}
